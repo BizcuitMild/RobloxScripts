5183407eb5fa8b9f534086b98d61283605611d4d
Picatchu. <72950145+BizcuitMild@users.noreply.github.com>
2026-08-14 16:47:50 +0700

    Update NewLib.lua

--- NewLib.lua
+++ NewLib.lua
@@ -37,7 +37,7 @@ local ProtectGui = protectgui or (syn and syn.protect_gui) or function()
 end
 
 local Themes = {
-    Names = {"Dark", "Darker", "AMOLED", "Light", "Balloon", "SoftCream", "Aqua", "Amethyst", "Rose", "Midnight",
+    Names = {"Dark", "Darker", "Zyrion", "AMOLED", "Light", "Balloon", "SoftCream", "Aqua", "Amethyst", "Rose", "Midnight",
              "Forest", "Sunset", "Ocean", "Emerald", "Sapphire", "Cloud", "Grape", "Bloody", "Arctic"},
     Dark = {
         Name = "Dark",
@@ -100,6 +100,32 @@ local Themes = {
         DialogBorder = Color3.fromRGB(50, 50, 50),
         DialogInput = Color3.fromRGB(45, 45, 45),
         DialogInputLine = Color3.fromRGB(120, 120, 120)
+    },
+    Zyrion = {
+        Name = "Zyrion",
+        Accent = Color3.fromRGB(255, 231, 195),
+        AcrylicMain = Color3.fromRGB(30, 30, 30),
+        AcrylicBorder = Color3.fromRGB(60, 60, 60),
+        AcrylicGradient = ColorSequence.new(Color3.fromRGB(17, 17, 17), Color3.fromRGB(18, 18, 18)),
+        AcrylicNoise = 0.94,
+        TitleBarLine = Color3.fromRGB(255, 231, 195),
+        Tab = Color3.fromRGB(255, 231, 195),
+        Element = Color3.fromRGB(70, 70, 70),
+        ElementBorder = Color3.fromRGB(25, 25, 25),
+        InElementBorder = Color3.fromRGB(55, 55, 55),
+        ElementTransparency = 0.82,
+        DropdownFrame = Color3.fromRGB(120, 120, 120),
+        DropdownHolder = Color3.fromRGB(35, 35, 35),
+        DropdownBorder = Color3.fromRGB(25, 25, 25),
+        Dialog = Color3.fromRGB(35, 35, 35),
+        DialogHolder = Color3.fromRGB(25, 25, 25),
+        DialogHolderLine = Color3.fromRGB(255, 231, 195),
+        DialogButton = Color3.fromRGB(35, 35, 35),
+        DialogButtonBorder = Color3.fromRGB(55, 55, 55),
+        DialogBorder = Color3.fromRGB(50, 50, 50),
+        DialogInput = Color3.fromRGB(45, 45, 45),
+        DialogInputLine = Color3.fromRGB(255, 231, 195),
+        Text = Color3.fromRGB(247, 227, 233)
     },
     AMOLED = {
         Name = "AMOLED",
